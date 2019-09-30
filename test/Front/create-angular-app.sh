d7906cedcfab4e710f67280579d9a6901208763f
afougerouse <alexis.fougerouse@lab-solutec.fr>
2019-09-30 11:46:52 +0200

    At times like this I wish I was a Garbage Man.

--- test/Front/create-angular-app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Crée une application Angular, nécessite l'installation de NodeJS
-# Angular empêche la création d'une application avec le nom "app"
-ng new appt && mv appt/ app
--- test/Front/update-modules.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Mets à jour les dépendances dans le container
-docker exec -it angular bash -c "npm i"
